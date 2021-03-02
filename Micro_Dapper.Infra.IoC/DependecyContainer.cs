0ff9978e582732e744850642937ffd1b4e3c65ac
Ali Ghasemzad <a.gh29@yahoo.com>
2021-03-02 22:02:23 +0330

    Initial Commit of Infrastructure Dependency Project

---
+++ Micro_Dapper.Infra.IoC/DependecyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Micro_Dapper.Domain.Core.Bus;
+using Micro_Dapper.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Micro_Dapper.Infra.IoC
+{
+    public class DependecyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
